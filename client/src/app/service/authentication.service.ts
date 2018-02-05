67766323ed2e539f63fb36eebbb5f0b863afb2aa
samuellundin <samuel.lundin21@gmail.com>
2018-02-05 09:43:42 +0100

    about

--- client/src/app/service/authentication.service.ts
+++ client/src/app/service/authentication.service.ts
@@ -38,7 +38,7 @@ export class AuthenticationService {
         this.router.navigate(['/home']);
       });
     }, error => {
-      console.log(error);
+      console.log(error.message);
     });
   }
 
